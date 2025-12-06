af0b623cd15b743ced8035f335a7989d9669060c
joeclack <joebclack@gmail.com>
2025-12-06 14:05:36 +0000

    Part 2

--- 2025/Day_2/main.go
+++ 2025/Day_2/main.go
@@ -8,14 +8,16 @@ import (
 )
 
 func main() {
-	total := 0
-
 	data, err := os.ReadFile("input.txt")
 	if err != nil {
 		panic(err)
 	}
 
 	values := strings.Split(strings.TrimSpace(string(data)), ",")
+
+	// part 1 - invalid id is string repeated i.e 1212 would be 12 repeated twice or 134134 is 134 repeated twice
+
+	total := 0
 
 	for _, v := range values {
 		sten := strings.Split(v, "-")
@@ -46,5 +48,57 @@ func main() {
 		total += valid
 	}
 
-	fmt.Println(total)
+	out := fmt.Sprintf("Part 1: %d", total)
+	fmt.Println(out)
+
+	// part 2 - invalid ids are now strings with repeated sequences of digits at leat twice
+	// i.e 121212 is 12 repeated 3 times or 1111 is 1 repeated 4 times
+
+	total = 0 // reset total
+
+	for _, v := range values {
+		sten := strings.Split(v, "-")
+		start, err := strconv.Atoi(sten[0])
+		if err != nil {
+			panic(err)
+		}
+		end, err := strconv.Atoi(sten[1])
+		if err != nil {
+			panic(err)
+		}
+
+		valid := 0
+		count := 0
+
+		for i := start; i <= end; i++ {
+			str := strconv.Itoa(i)
+			le := len(str)
+			max := le / 2
+			for pat := 1; pat <= max; pat++ {
+				if le%pat != 0 {
+					continue
+				}
+
+				pattern := str[:pat]
+				repeatCount := le / pat
+
+				repeated := strings.Repeat(pattern, repeatCount)
+
+				if repeated == str {
+					if repeatCount >= 2 {
+						valid += i
+						count++
+						pat = max + 1 // now this number has been identified, it doesnt need checking again.
+						// 111111 would be flagged as invalid 3 times cause 1 repeated 6 times, 11 repeated 3 times and 111 repeated 2 times.
+						// this should only be glagged once so move onto the next number in the range
+					}
+				}
+			}
+		}
+
+		total += valid
+	}
+
+	out2 := fmt.Sprintf("Part 2: %d", total)
+	fmt.Println(out2)
 }
